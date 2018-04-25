b959f501433041e50a091be5400336ed67d489d2
2toetommy <2toetommy@gmail.com>
2018-04-24 17:22:49 -0700

    .
    .

--- Chronik Bot/scrape.py
+++ Chronik Bot/scrape.py
@@ -1,17 +1,25 @@
 import requests
 from bs4 import BeautifulSoup as bs
+import json
 import discord
 import logging
 cFile = open('postList.txt', 'w')
 aFile = open('postList.txt', 'a')
+
+
 url = "https://steemit.com/@chronik-n-coffee"
 r = requests.get(url)
+result = json.load("https://steemit.com/@chronik-n-coffee")
+print('_______')
+print(result['result']['content']['body'])
+
 soup = bs(r.content, "html.parser")
 
 p_title = soup.find("div", {"class": "articles__summary"})
 p_image = soup.find("div", {"class": "articles__content-block--img"})
 p_author = soup.find("div", {"class": "author"})
 
+#Get Title and Img
 for container in p_title:
     i_title = container.contents[1].text
     print(i_title)
@@ -19,6 +27,7 @@ for container in p_title:
     aFile.write(str(i_title))
 for item in p_image:
     i_image = item.contents[0].find_all("img")
-    print(i_image)
+    s_image = i_image
+    print(s_image)
     aFile.write('\n')
     aFile.write(str(i_image))
